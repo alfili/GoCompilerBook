bf23441c98c21da3c804ea33a941d9007920cf55
alfili <146325432+alfili@users.noreply.github.com>
2024-03-15 04:58:50 +0700

    init interpriter (tokens, lexer with tests)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gocompilerbook
+
+go 1.21.1
---
+++ lexer/lexer_test.go
@@ -0,0 +1,74 @@
+package lexer
+
+import (
+	"gocompilerbook/token"
+	"testing"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `let x = 5; let y = 10;
+	
+	let add = fn(a, b) {
+		a + b;
+	};
+
+	let result = add(x, y);
+	`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.LET, "let"},
+		{token.IDENTIFIER, "x"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "y"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENTIFIER, "a"},
+		{token.COMMA, ","},
+		{token.IDENTIFIER, "b"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENTIFIER, "a"},
+		{token.PLUS, "+"},
+		{token.IDENTIFIER, "b"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENTIFIER, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENTIFIER, "add"},
+		{token.LPAREN, "("},
+		{token.IDENTIFIER, "x"},
+		{token.COMMA, ","},
+		{token.IDENTIFIER, "y"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - token wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - token wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
